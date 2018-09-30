0fb768202ee926906d1f6f52c20fc2869537696c
A.Star <astar@snowland.ltd>
2018-09-30 09:06:39 +0800

    update APPClient

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ cpabe-java
 ==========
 
 The implementation of Ciphertext Policy Attribute Based Encryption in Java.
+
+2018-9-25 陈小龙 增加SM3,SM4扩展原有功能
--- src/cn/com/wasec/app/APPClient.java
+++ src/cn/com/wasec/app/APPClient.java
@@ -1,5 +1,12 @@
 package cn.com.wasec.app;
 
-public class APPClient {
+import javax.swing.JFrame;
+
+public class APPClient extends JFrame{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6549926805824684244L;
 	
 }
