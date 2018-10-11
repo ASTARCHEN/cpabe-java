5c6955cd040b72d2440f12f5ce31c20fc2169ac4
A.Star <astar@snowland.ltd>
2018-10-11 12:16:25 +0800

    增加图形界面，修改基础类NumberTool

--- src/cn/com/wasec/app/APPClient.java
+++ src/cn/com/wasec/app/APPClient.java
@@ -1,12 +1,109 @@
 package cn.com.wasec.app;
 
-import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
-public class APPClient extends JFrame{
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JList;
+import javax.swing.JTextArea;
+import javax.swing.JTree;
+
+/**
+ * @author admin
+ *
+ */
+public class APPClient extends JFrame {
 
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = -6549926805824684244L;
-	
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JButton btnGo;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					APPClient frame = new APPClient();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public APPClient() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 579, 409);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(31, 62, 147, 21);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(31, 103, 147, 21);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JButton btnNewButton = new JButton("update");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(31, 229, 93, 23);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("...");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_1.setBounds(195, 61, 93, 23);
+		contentPane.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("...");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_2.setBounds(195, 102, 93, 23);
+		contentPane.add(btnNewButton_2);
+		
+		btnGo = new JButton("go!");
+		btnGo.setBounds(31, 196, 93, 23);
+		contentPane.add(btnGo);
+		
+		JList list = new JList();
+		list.setBounds(361, 64, 162, 188);
+		contentPane.add(list);
+		
+		JTree tree = new JTree();
+		tree.setBounds(361, 64, 78, 64);
+		contentPane.add(tree);
+		
+		JTree tree_1 = new JTree();
+		tree_1.setBounds(361, 174, 78, 64);
+		contentPane.add(tree_1);
+	}
 }
